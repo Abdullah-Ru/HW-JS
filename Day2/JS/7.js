7b45876da8bc13369ba4447407f647eea6aaed10
Abdullah <a.az119@hotmail.com>
2023-08-07 18:51:59 +0300

    Add first Homework

---
+++ Day2/JS/7.js
@@ -0,0 +1,16 @@
+// First Homework
+let age;
+age = 17;
+let Name;
+Name = "Abdullah";
+console.log(Name , age)
+// Second Homework
+age = 16
+console.log(age);
+var $age = 13;
+console.log($age);
+var $age = 12;
+console.log($age)
+//
+const Age = 15;
+Age = 14; // Error
