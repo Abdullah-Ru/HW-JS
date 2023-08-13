533f834e738904196ff269277a87460325a14753
Abdullah <a.az119@hotmail.com>
2023-08-13 19:55:00 +0300

    Add Day6 homework

---
+++ Day6/practice.js
@@ -0,0 +1,11 @@
+let names = ["علي", "سماح", "خالد", "فهد", "محمد"];
+console.log(names.slice(2,3)); 
+// or
+console.log(names[2]);
+////////////////////////////////////////////////////
+names.unshift("عبدالله");
+names.push("ريان");
+console.log(names);
+names.shift()
+names.pop()
+console.log(names);
