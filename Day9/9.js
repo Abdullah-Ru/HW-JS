d8e90ebfd84afe655a621be45f97ed1e92a4c0f4
Abdullah <a.az119@hotmail.com>
2023-08-17 09:33:53 +0300

    Add day9 homework

---
+++ Day9/9.js
@@ -0,0 +1,24 @@
+// الواجب الاول
+for (let i = 1; i <= 30; i++) {
+    if (i <= 10) {
+        console.log(i);
+    }
+}
+// الواجب الثاني
+for (let i = 1; i <= 20; i++) {
+    if (i - 10) {
+        console.log(i);
+    }
+}
+// الواجب الثالث
+let x = 3;
+while (x >= 1) {
+    console.log(x);
+    x--;
+}
+// الواجب الرابع
+let y = 0;
+do {
+    console.log(y);
+    y++
+} while (y <=5);
